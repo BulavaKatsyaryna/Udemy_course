9a08ee2a8a738975433db95e6fe5acea18635849
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-09-19 01:03:49 +0300

    working with the type Advice - AfterReturning

--- Udemy_course/src/main/java/aop/aspects/UniversityLoggingAspect.java
+++ Udemy_course/src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -1,9 +1,12 @@
 package aop.aspects;
 
+import aop.Student;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Aspect
@@ -14,8 +17,19 @@ public class UniversityLoggingAspect {
         System.out.println("beforeGetStudentsLoggingAdvice: logging the receipt of the list of students before the getStudents method");
     }
 
-    @AfterReturning("execution(* getStudents())")
-    public void afterReturningGetStudentsLoggingAdvice() {
+    @AfterReturning(pointcut = "execution(* getStudents())"
+            ,returning = "students")
+    public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {
+        Student firstStudent = students.get(0);
+
+        String nameSurname = firstStudent.getNameSurname();
+        nameSurname = "Mrs. " + nameSurname;
+        firstStudent.setNameSurname(nameSurname);
+
+        double avgGrade = firstStudent.getAvgGrade();
+        avgGrade = avgGrade + 1;
+        firstStudent.setAvgGrade(avgGrade);
+
         System.out.println("afterReturningGetStudentsLoggingAdvice: logging the receipt of the list of students after working out the getStudents method");
     }
 }
