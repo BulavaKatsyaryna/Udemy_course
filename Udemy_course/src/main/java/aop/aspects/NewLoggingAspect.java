338ad017de2e88b201e11a61d1b889ae25d80ad4
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-09-22 01:03:29 +0300

    Measured how long the proceedingJoinPoint method takes

--- Udemy_course/src/main/java/aop/aspects/NewLoggingAspect.java
+++ Udemy_course/src/main/java/aop/aspects/NewLoggingAspect.java
@@ -14,9 +14,14 @@ public class NewLoggingAspect {
 
         System.out.println("aroundReturnBookLoggingAdvice: Trying to return a book to the library");
 
+        Long begin = System.currentTimeMillis();
+
         Object targetMethodResult = proceedingJoinPoint.proceed();
 
+        Long end = System.currentTimeMillis();
+
         System.out.println("aroundReturnBookLoggingAdvice: The book was successfully returned to the library");
+        System.out.println("aroundReturnBookLoggingAdvice: Method returnBook did the job for " + (end - begin) + " milliseconds");
 
         return targetMethodResult;
     }
